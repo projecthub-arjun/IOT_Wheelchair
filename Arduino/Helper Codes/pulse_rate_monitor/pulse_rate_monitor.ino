97031bbc9ae3236521eff74511f42b802474aeb1
projecthub-arjun <masterarjunsuresh@gmail.com>
2015-12-26 19:49:22 +0530

    Adjusted pulse rate monitor code to only display beats per minutes in serial monitor

--- Arduino/Helper Codes/pulse_rate_monitor/pulse_rate_monitor.ino
+++ Arduino/Helper Codes/pulse_rate_monitor/pulse_rate_monitor.ino
@@ -80,8 +80,10 @@ void ledFadeToBeat(){
 
 
 void sendDataToProcessing(char symbol, int data ){
+  
+    if(symbol == 'B'){
     Serial.print(symbol);                // symbol prefix tells Processing what type of data is coming
-    Serial.println(data);                // the data to send culminating in a carriage return
+    Serial.println(data);}                // the data to send culminating in a carriage return
   }
 
 
